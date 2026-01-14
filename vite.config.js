b81825fa02aa928c3ec3abeace0eef290bc8da17
joboy05 <jolidonhoungue30@gmail.com>
2026-01-14 14:26:11 +0100

    modifs

--- vite.config.js
+++ vite.config.js
@@ -2,19 +2,20 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
-    // Vérifiez les paramètres de build
+  plugins: [react()],
   build: {
     outDir: 'dist',
-    // ...
-  }
-  plugins: [react()],
+  },
   server: {
     proxy: {
+      // Configuration du proxy pour l'API
       '/api': {
         target: 'http://localhost:5000',
         changeOrigin: true,
       },
+      // Configuration du proxy pour les uploads
       '/uploads': {
         target: 'http://localhost:5000',
         changeOrigin: true,
