ec10b8c2d585021a804855190562b235e8142b0b
joboy05 <jolidonhoungue30@gmail.com>
2026-01-14 14:18:03 +0100

    bofbof

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,11 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+    // Vérifiez les paramètres de build
+  build: {
+    outDir: 'dist',
+    // ...
+  }
   plugins: [react()],
   server: {
     proxy: {
